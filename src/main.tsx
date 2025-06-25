989461bcc01c2edb48e54180c05f39a9b0946a1f
Muhammad Aashir Khan <MAashirkhan467@gmail.com>
2025-06-25 11:18:14 +0500

    feat: added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import ReactGA from 'react-ga4';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactGA.initialize('G-XBE3PVRF3W');
 ReactGA.send('pageview');
@@ -10,5 +11,6 @@ ReactGA.send('pageview');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App />
+      <Analytics />
   </StrictMode>,
 )
